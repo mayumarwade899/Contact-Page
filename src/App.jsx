2a78e503518d7c6bf61a0f468d35432579a5dbcf
Mayur Marwade <mayumarwade899@gmail.com>
2024-05-05 12:09:53 +0530

    nav bar added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./components/navigation/Navigation";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
